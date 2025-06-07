981da26a1b3a9211db8d437528530c429829b64e
Mr Punk da Silva <gustavojs417@gmail.com>
2025-06-07 16:53:01 -0300

    added icons

---
+++ src/views/Home/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Hero from '../components/Hero.vue'
+import Separator from "@/components/Separator.vue";
+</script>
+
+<template>
+  <main>
+
+    <Hero/>
+
+    <Separator />
+
+    
+
+  </main>
+</template>
