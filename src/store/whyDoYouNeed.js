5a0d090b4bea6b4adba26bc128941638ccbd7fbe
Mr Punk da Silva <gustavojs417@gmail.com>
2025-06-19 19:55:30 -0400

    refactor styles from why do you need

--- src/store/whyDoYouNeed.js
+++ src/store/whyDoYouNeed.js
@@ -5,20 +5,20 @@ import Image3 from '@/assets/img/homeSections/whyDoYouNeed/three-business-cats-o
 export const WHY_DO_YOU_NEED = [
   {
     id: 1,
-    title: 'lorem ipsum dolor sit amet',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis',
+    title: ' Entenda Exatamente o Que Esperam de Você',
+    description: ' Diga adeus às incertezas! O Guma Agent analisa a fundo as rubricas do Canvas e compara com sua entrega, mostrando com clareza o que falta e o que já está perfeito. Receba orientações diretas e pontuais para cada tarefa.',
     img: Image1,
   },
   {
     id: 2,
-    title: 'lorem ipsum dolor sit amet',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis',
+    title: 'Feedback Inteligente, Resultados Surpreendentes',
+    description: 'Mais do que uma correção, um verdadeiro mentor. Nossa IA avançada identifica pontos fortes e áreas de melhoria, sugerindo ajustes específicos para que você aprimore seu trabalho antes da entrega e supere as expectativas.',
     img: Image2,
   },
   {
     id: 3,
-    title: 'lorem ipsum dolor sit amet',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis',
+    title: 'Domine Suas Entregas e Suas Notas',
+    description: 'Pare de adivinhar e comece a gabaritar! Com o Guma Agent, você desenvolve habilidades de escrita e argumentação, otimiza seu tempo de estudo e garante as melhores notas, transformando seu desempenho acadêmico de uma vez por todas.',
     img: Image3,
   },
 ];
