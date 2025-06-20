d2ca8de3d37ab74016cf868d80cf43074e2366ee
Mr Punk da Silva <gustavojs417@gmail.com>
2025-06-19 21:38:15 -0400

    refactor styles from is that for me

--- src/views/Home/components/SectionIsThatForMe.vue
+++ src/views/Home/components/SectionIsThatForMe.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="main-content">
+  <section class="is-that-for-me">
     <header>
       <h2>Is that for <span class="text-accent">me</span>?</h2>
     </header>
@@ -10,9 +10,16 @@
       </aside>
 
       <article>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt in officiis possimus.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque odit quae repudiandae!</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque odit quae repudiandae!</p>
+        <p>
+          Sim! O <strong>Guma Agent</strong> foi desenvolvido para qualquer <em>estudante universitário</em> que busca
+          aprimorar suas entregas e alcançar <strong>melhores resultados</strong>.
+        </p>
+        <p>
+          É para você que fica <strong>perdido nas rubricas e critérios de avaliação do Canvas</strong>, para quem
+          deseja um <em>feedback detalhado e personalizado</em> antes de entregar suas tarefas, e para quem quer, de
+          fato, <strong>melhorar suas notas</strong>, <strong>otimizar o tempo de estudo</strong> e ter a certeza de que
+          está sempre entregando o <strong>seu melhor trabalho</strong> em todas as disciplinas.
+        </p>
       </article>
     </div>
   </section>
